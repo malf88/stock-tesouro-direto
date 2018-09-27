903f3fe7a41abc38687fb38442ea0292f90144ab
Marco Aurélio Lima Fernandes <malf88@gmail.com>
2018-09-27 12:20:49 -0300

    Alteração no index.

--- index.php
+++ index.php
@@ -10,4 +10,4 @@ include 'vendor/autoload.php';
 $foo = new StockTesouroDireto\StockTesouroDireto('CPF','SENHA');
 //var_dump($foo->getTitulos());
 var_dump($foo->findTitulo(\StockTesouroDireto\StockTesouroDireto::TESOURO_IPCA_2045));
-var_dump($foo->getTitulos(\StockTesouroDireto\StockTesouroDireto::TESOURO_IPCA_2045));
+var_dump($foo->getTitulos());
